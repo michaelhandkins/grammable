a8f552ed2975460342ce6d247e891752b6f2c3d7
Michael Handkins <michaelhandkins@aol.com>
2019-07-09 21:47:49 +0000

    Made it so grams can successfully be updated

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+describe "grams#update action" do
+  it "should successfully update the gram if the gram is found" do
+    gram = FactoryBot.create(:gram, message: "Initial Value")
+    patch :update, params: {id: gram.id, gram: {message: "Changed"}}
+    expect(response).to redirect_to root_path
+    gram.reload
+    expect(gram.message).to eq "Changed"
+  end
+
+  it "should return a 404 error message if the gram is not found" do
+    patch :update, params: {id: "YOLOSWAG", gram: {message: "Changed"}}
+    expect(response).to have_http_status(:not_found)
+  end
+
+  it "should return to the edit form with a status of unprocessable_entity if there are validation errors" do
+    gram = FactoryBot.create(:gram, message: "Initial Value")
+    patch :update, params: {id: gram.id, gram: {message: ""}}
+    expect(response).to have_http_status(:unprocessable_entity)
+    gram.reload
+    expect(gram.message).to eq "Initial Value"
+  end
+end
+
 describe "grams#edit action" do
   it "should successfully show the edit page if the gram is found" do
     gram = FactoryBot.create(:gram)
