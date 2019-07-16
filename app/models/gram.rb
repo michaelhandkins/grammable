f071c8613a18504905cb59b531529faa70be5316
Michael Handkins <michaelhandkins@aol.com>
2019-07-16 21:34:27 +0000

    styled the app

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,7 @@
 class Gram < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
+  has_many :comments
   
   validates :picture, presence: true
   validates :message, presence: true
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     it "should return http status code of not found if the gram isn't found" do
-      user = FactoryBot.create(user)
+      user = FactoryBot.create(:user)
       sign_in user
       post :create, params: {gram_id: "YOLOSWAG", comment: {message: "awesome gram"}}
       expect(response).to have_http_status :not_found
